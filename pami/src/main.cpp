350862f08f937157c3c04e43e38c5a8a4b0a25fb
arduilol <equipe@electrobot.fr>
2026-04-04 17:29:32 +0200

    pami 2 3

--- pami/src/main.cpp
+++ pami/src/main.cpp
@@ -229,21 +229,30 @@ void loop() {
             // 10 000 = 123cm, PAMI 1: 6097, PAMI 2: 13089, PAMI 3: 9593
             avancer(3000);
             delay(10);
-                tourner_droite(200);
-            avancer(9512);
-            delay(10);
-                tourner_droite(250);
-            avancer(5000);
+                tourner_gauche(300);
+            avancer(6000);
         } else {
             avancer(3000);
             delay(10);
-                tourner_gauche(200);
-            avancer(9512);
-            delay(10);
-                tourner_gauche(250);
-            avancer(5000);
+                tourner_droite(300);
+            avancer(6000);
         }
 #endif
+#ifdef PAMI3
+        if (equipe == BLEU) {
+            // 10 000 = 123cm, PAMI 1: 6097, PAMI 2: 13089, PAMI 3: 9593
+            avancer(3000);
+            delay(10);
+                tourner_gauche(190);
+            avancer(9512);
+        } else {
+            avancer(3000);
+            delay(10);
+                tourner_droite(190);
+            avancer(9512);
+        }
+#endif
+
 
         mouvement_termine = true;
     }
